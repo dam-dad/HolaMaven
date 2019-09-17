a14e4fcdfd711a6c6a4941066a9a5d86549fdc4c
Fran Vargas <francisco.vargas.ruiz@gmail.com>
2019-09-17 12:25:22 +0100

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,15 @@
+package dad.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String greeting = StringUtils.reverse("Hola Maven!");
+		
+		System.out.println(greeting);
+		
+	}
+
+}
